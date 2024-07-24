ee5e60c968be80b8915f464d7783c93cb9a0f018
KaidRommel <18939774762@163.com>
2024-07-24 10:24:49 +0800

    Display Keypresses

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io::{stdin, stdout, Read};
+use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
+
+
 fn main() {
-    println!("Hello, world!");
+    enable_raw_mode().unwrap();
+    for input in stdin().bytes() {
+        let input = input.unwrap();
+        let char = input as char;
+        if char == 'q' {
+            disable_raw_mode().unwrap();
+            return;
+        } else if char.is_control() {
+            println!("Binary: {0:08b} ASCII: {0:#03} \r",input);
+        } else {
+            println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r",input, char)
+        }
+    }
 }
